d73d71eb2290b2c19253a13970477d5918fd885a
Samael Home <samaelortiz2218@gmail.com>
2026-01-13 14:56:00 -0800

    feat: add auth controller and routes

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,9 @@
+import type { Request, Response } from "express";
+
+export const register = (req: Request, res: Response) => {
+    res.json({ message: "Register" });
+};
+
+export const login = (req: Request, res: Response) => {
+    res.json({ message: "Login" });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { config } from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
 
 config();
 
 const app = express();
 app.use(express.json());
+
+app.use("/", authRoutes);
 
 app.listen(process.env.PORT || 5173, () => {
     console.log(`Server running on port ${process.env.PORT}`);
