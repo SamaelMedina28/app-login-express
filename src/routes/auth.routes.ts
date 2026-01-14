8bb25a413c45fbdf37a94e40c3aa4eb0a2930b96
Samael Home <samaelortiz2218@gmail.com>
2026-01-13 19:52:58 -0800

    feat: implements route to get user info

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,7 +2,8 @@ import express from "express";
 import { register, login, logout } from "../controllers/auth.controller.js";
 import { registerRules, loginRules } from "../validations/auth.validations.js";
 import { validate } from "../middlewares/validate.js";
-
+import { isAuth } from "../middlewares/isAuth.js";
+import type { Request, Response } from "express";
 const router = express.Router();
 
 router.post("/register", registerRules, validate, register);
@@ -10,5 +11,11 @@ router.post("/register", registerRules, validate, register);
 router.post("/login", loginRules, validate, login);
 
 router.post("/logout", logout);
+
+router.use(isAuth);
+
+router.get("/profile", (req: Request, res: Response) => {
+    res.json({ user: req.user });
+});
 
 export default router;
