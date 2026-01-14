c084bf805c113fb5d8f5e7bba00213477a45f8d6
Samael Home <samaelortiz2218@gmail.com>
2026-01-13 19:12:36 -0800

    feat: add type/coockie parser

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import express from "express";
 import { config } from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import { conectDB } from "./config/db.js";
-
+import cookieParser from "cookie-parser";
 config();
 conectDB();
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/", authRoutes);
 
