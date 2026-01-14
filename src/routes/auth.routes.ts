81b20ea085cfc684f2a1a566728679c4eace9037
Samael Home <samaelortiz2218@gmail.com>
2026-01-13 19:36:09 -0800

    feat: create LotOut

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login } from "../controllers/auth.controller.js";
+import { register, login, logout } from "../controllers/auth.controller.js";
 import { registerRules, loginRules } from "../validations/auth.validations.js";
 import { validate } from "../middlewares/validate.js";
 
@@ -8,5 +8,7 @@ const router = express.Router();
 router.post("/register", registerRules, validate, register);
 
 router.post("/login", loginRules, validate, login);
+
+router.post("/logout", logout);
 
 export default router;
