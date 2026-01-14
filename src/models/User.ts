22dbde3aca7687718f9f6be59aaaa2edbb3217ef
Samael Home <samaelortiz2218@gmail.com>
2026-01-13 18:34:20 -0800

    feat: add timestamps to User Schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -14,6 +14,9 @@ const userSchema = new Schema({
         type: String,
         required: true,
     },
+}, {
+    timestamps: true,
+    collection: "users",
 });
 
 export default model("User", userSchema);
