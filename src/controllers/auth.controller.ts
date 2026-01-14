ea315d8586c620dad440a21aac836b9915f99708
Samael Home <samaelortiz2218@gmail.com>
2026-01-13 18:34:38 -0800

    feat: register user method

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,10 +1,15 @@
 import type { Request, Response } from "express";
 import { validationResult } from "express-validator";
+import { hashPassword } from "../utils/hashBcrypt.js";
+import User from "../models/User.js";
 
-export const register = (req: Request, res: Response) => {
-    res.json({ message: "Register" });
+export const register = async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+  const hashedPassword = hashPassword(password);
+  const user = await User.create({ name, email, password: hashedPassword });
+  res.status(201).json({ message: "User created", user });
 };
 
 export const login = (req: Request, res: Response) => {
-    res.json({ message: "Login" });
+  res.status(200).json({ message: "Login" });
 };
