6d8983e735fe55b3631de9a9782b824bcd631d31
Samael Home <samaelortiz2218@gmail.com>
2026-01-16 11:22:11 -0800

    feat: add cors to middlewares

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,17 @@ import { config } from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import { conectDB } from "./config/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 config();
 conectDB();
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 
 app.use("/", authRoutes);
 
